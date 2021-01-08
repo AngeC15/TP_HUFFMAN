2c6ad41fb38b5f82c462fafc7e5ffe2044f57a7a
Cyprien Pelisse-Verdoux <cyprien.pelisse-verdoux@etu.univ-grenoble-alpes.fr>
2021-01-08 15:21:51 +0100

    fonction initHuffman et ConstruireTableOcc

--- huff_encode.c
+++ huff_encode.c
@@ -22,6 +22,10 @@ void ConstruireTableOcc(FILE *fichier, TableOcc_t *TableOcc) {
     c = fgetc(fichier);
     while (c != EOF) {
         /* A COMPLETER ... */
+        //les char sont des entier donc à l'indice de l'entier on ajoute 1 pour chaque occurence
+        //if( 0<c && c < 255){
+            printf("%d \n ", c);
+            TableOcc->tab[c]++;
         c = fgetc(fichier);
     };
 
@@ -36,13 +40,29 @@ void ConstruireTableOcc(FILE *fichier, TableOcc_t *TableOcc) {
 
 fap InitHuffman(TableOcc_t *TableOcc) {
     /* A COMPLETER */
-    printf("Programme non realise (InitHuffman)\n");
-    return NULL;
+    //je parcours le tableau et je mets chaque élément à 0
+    //printf("Programme non realise (InitHuffman)\n");
+    //nouvelle FAP:
+    fap new_fap;
+    //pour chaque element de la tableOcc
+    for(int i =0; i<256; i++){
+        
+        if(TableOcc->tab[i] > 0) {
+            //si le nombre d'occurence est trop petit( 0 ou moins) on ne le mets pas dans la fap
+            //on cree un arbe à partir de i
+            Arbre n_elem = {i, NULL, NULL};
+            //on ajoute cet arbre dans la FAP
+            inserer(new_fap, n_elem , TableOcc->tab[i]);
+        }
+    }
+    //on doit return une struct fap
+    return new_fap;
 }
 
 Arbre ConstruireArbre(fap file) {
     /* A COMPLETER */
     printf("Programme non realise (ConstruireArbre)\n");
+    //on doit return un struct Arbre
     return ArbreVide();
 }
 
@@ -63,9 +83,11 @@ int main(int argc, char *argv[]) {
     FILE *fichier;
     FILE *fichier_encode;
 
+    //on ouvre le fichier
     fichier = fopen(argv[1], "r");
     /* Construire la table d'occurences */
     ConstruireTableOcc(fichier, &TableOcc);
+    //il ferme le fichier
     fclose(fichier);
 
     /* Initialiser la FAP */
